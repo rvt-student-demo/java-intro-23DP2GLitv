7fe611862759c30c667c70db7354916cff22323e
23DP2GLitv <166109634+23DP2GLitv@users.noreply.github.com>
2024-09-05 09:50:41 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,7 +9,13 @@ public class App
         Scanner scanner = new Scanner(System.in);
 
         // Mēs gribam uzzinat no lietotaja vardu, uzvardu un gruppu
-
+        System.out.println("Ievadi savu vārdu: ");
+        String name = scanner.nextLine();
+        System.out.println("Ievadi savu uzvārdu: ");
+        String second_name = scanner.nextLine();
+        System.out.println("Ievadi savu gruppu: ");
+        String group = scanner.nextLine();
         // Un beigās printēt visu kopā vienā rindiņā izmantojot String savienošanu
+        System.out.println(name + " " + second_name + " " + group);
     }
 }
