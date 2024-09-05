447cdc2d5cca8dff6550814f46946bb104184da8
23DP2GLitv <166109634+23DP2GLitv@users.noreply.github.com>
2024-09-05 09:42:19 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,15 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello RVT!");
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        // Mēs gribam uzzinat no lietotaja vardu, uzvardu un gruppu
+
+        // Un beigās printēt visu kopā vienā rindiņā izmantojot String savienošanu
+    }
 }
