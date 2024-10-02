12736e411cb41dad03c6782473b570f7f6193495
23DP2GLitv <166109634+23DP2GLitv@users.noreply.github.com>
2024-10-02 08:26:16 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,35 +7,18 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int numbers = 0;
-        int odd = 0;
-        int even = 0;
-        System.out.println("Give numbers: ");
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == -1) {
-                break;
-            }
-            sum = sum + number;
-            numbers++;
-            if (number % 2 == 0) {
-                even++;
-            }
-            else {
-                odd++;
-            }
-
-
+        System.out.println("How many times?");
+        int number = Integer.valueOf(scanner.nextLine());
+        for (int i = 1; i < number + 1; i++) {
+            printText();
         }
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + numbers);
-        System.out.println("Average: " + (double)sum / numbers);
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
+
         
 }
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+}
+    
 }
 
     
