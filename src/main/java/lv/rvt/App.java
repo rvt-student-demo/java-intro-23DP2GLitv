648aab0d3a126df0e88bcd211bd84da068f8d2cc
23DP2GLitv <166109634+23DP2GLitv@users.noreply.github.com>
2024-10-17 08:33:47 +0000

    Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,18 +7,36 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
+        Random rand = new Random();
 
-        int summa = 0;
+        int val = rand.nextInt(10)+1;
+        int three_try = 3;
+        
+        System.out.println("I am thinking of a number from 1 to 10.");
+        System.out.println("You must guess what it is in three tries.");
+        System.out.println("Enter a guess:  ");
+        
         while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == 0) {
+            int guess = Integer.valueOf(scanner.nextLine());
+            if (guess == val) {
+                System.out.println("RIGHT!");
+                System.out.println("You have won the game.");
                 break;
             }
-            summa = summa + number;
-
+            else {
+                System.out.println("wrong");
+                three_try--;
+                if (three_try == 0) {
+                    System.out.println("The correct number was " + val + ".");
+                    System.out.println("You have lost the game.");
+                    break;
+                }
+            }
+            
+        
         }
-        System.out.println(summa);
+        
+
 
         
 
