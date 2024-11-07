08c8a5b005085309ab13147afc2d5d1a7eacc9a3
23DP2GLitv <166109634+23DP2GLitv@users.noreply.github.com>
2024-11-07 09:23:11 +0000

    Statistics

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,21 +7,31 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        PaymentCard paulsCard = new PaymentCard(20);
-        PaymentCard mattsCard = new PaymentCard(30);
-        paulsCard.eatHeartily();
-        mattsCard.eatAffordably();
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-        paulsCard.addMoney(20);
-        mattsCard.eatHeartily();
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-        paulsCard.eatAffordably();
-        paulsCard.eatAffordably();
-        mattsCard.addMoney(50);
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
+        Statistics statistics = new Statistics();
+        System.out.println("Enter numbers: ");
+        int odd = 0;
+        int even = 0;
+        while (true) {
+            int skaitlis = Integer.valueOf(scanner.nextLine());
+            
+            if (skaitlis == -1) {
+                break;
+            }
+
+            else {
+                statistics.addNumber(skaitlis);
+                if (skaitlis % 2 == 0) {
+                    odd = odd + skaitlis;
+                }
+                else {
+                    even = even + skaitlis;
+                }
+            }
+        }
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of odd numbers: " + odd);
+        System.out.println("Sum of even numbers: " + even);
+    
 }
                   
 }
