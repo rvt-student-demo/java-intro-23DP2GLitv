d2b357c1e64df1621e0bc0a57ee68f55484e27cc
23DP2GLitv <166109634+23DP2GLitv@users.noreply.github.com>
2024-11-20 10:53:01 +0000

    PersonManager

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,26 +8,10 @@ public class App
 {
     public static void main( String[] args) throws Exception
     {
-        BufferedReader reader = Utils.getReader("persons.csv");
-        ArrayList<Person> persons = new ArrayList<>();
-        
-        double average = 0;
-        String line;
-        reader.readLine();
-
-
-        while ((line = reader.readLine()) != null) {
-            String[] parts = line.split(", ");
-            /* System.out.println("Array: " + Arrays.toString(parts)); */
-            Person person = new Person(parts[0], Integer.valueOf(parts[1]), Integer.valueOf(parts[2]), Integer.valueOf(parts[3]));
-            /* System.out.println("CSV line: " + line); */
-            persons.add(person);
-        }
+        ArrayList<Person> persons = PersonManager.getPersonList();
         for (Person person : persons) {
             System.out.println(person);
-            average = average + person.returnAge();
         }
-        System.out.println("Average age: " + average / 4);
 
 
   
